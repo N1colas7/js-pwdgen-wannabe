e8a4be862134995eec4fd8ca426aa7b3a5350d69
N1colas7 <nicolasnd9897@gmail.com>
2022-11-24 18:35:13 +0100

    Finish ex

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nome = (prompt ('Inserisci il tuo nome'));
+
+let cognome = (prompt('Inserisci il tuo cognome'));
+
+let colore = (prompt('Inserisci il tuo colore preferito'));
+
+document.getElementById("text") .innerHTML = 'La tua nuova password &eacute; ' + nome + cognome + colore + 21
